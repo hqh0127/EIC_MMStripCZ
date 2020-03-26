7d8d0f735c857b9776a00cf77b020c3e5af45c24
Huang Qinhua <qinhua.huang@polytechnique.edu>
2020-03-26 16:40:10 +0100

    Initial commit

---
+++ source/PHG4CylinderStripDetector.cc
@@ -0,0 +1,107 @@
+#include "PHG4CylinderStripDetector.h"
+
+#include <g4main/PHG4Detector.h>       // for PHG4Detector
+#include <g4main/PHG4DisplayAction.h>  // for PHG4DisplayAction
+#include <g4main/PHG4Subsystem.h>
+
+#include <Geant4/G4LogicalVolume.hh>
+#include <Geant4/G4Material.hh>
+#include <Geant4/G4PVPlacement.hh>
+#include <Geant4/G4PhysicalConstants.hh>
+#include <Geant4/G4String.hh>  // for G4String
+#include <Geant4/G4SystemOfUnits.hh>
+#include <Geant4/G4ThreeVector.hh>  // for G4ThreeVector
+#include <Geant4/G4Tubs.hh>
+#include <Geant4/G4UserLimits.hh>
+
+#include <TSystem.h>
+
+#include <cmath>
+#include <iostream>  // for operator<<, endl, basic_ost...
+#include <sstream>
+
+class G4VSolid;
+class PHCompositeNode;
+
+using namespace std;
+
+//_______________________________________________________________
+PHG4CylinderStripDetector::PHG4CylinderStripDetector(PHG4Subsystem *subsys, PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam, const int lyr
+  : PHG4Detector(subsys, Node, dnam)
+  , m_Params(parameters)
+  , m_CylinderPhysicalVolume(nullptr)
+  , m_Layer(lyr)
+{
+}
+
+//_______________________________________________________________
+bool PHG4CylinderStripDetector::IsInCylinder(const G4VPhysicalVolume *volume) const
+{
+  if (volume == m_CylinderPhysicalVolume)
+  {
+    return true;
+  }
+  return false;
+}
+
+//_______________________________________________________________
+void PHG4CylinderStripDetector::ConstructMe(G4LogicalVolume *logicWorld)
+{
+  G4Material *TrackerMaterial = G4Material::GetMaterial(m_Params->get_string_param("material"));
+
+  if (!TrackerMaterial)
+  {
+    std::cout << "Error: Can not set material" << std::endl;
+    gSystem->Exit(1);
+  }
+
+  // determine length of cylinder using PHENIX's rapidity coverage if flag is true
+  double radius = m_Params->get_double_param("radius") * cm;
+  double thickness = m_Params->get_double_param("thickness") * cm;
+  double gap = m_Params->get_double_param("gap") * cm;
+  G4VSolid *cylinder_solid = new G4Tubs(G4String(GetName()),
+                                        radius,
+                                        radius + thickness*2 + gap,
+                                        m_Params->get_double_param("length") * cm / 2., 0, twopi);
+  G4VSolid *cylinder_solid_C = new G4Tubs(G4String(GetName()),
+                                        radius,
+                                        radius + thickness,
+                                        m_Params->get_double_param("length") * cm / 2., 0, twopi);
+  G4VSolid *cylinder_solid_Z = new G4Tubs(G4String(GetName()),
+                                        radius + thickness + gap,
+                                        radius + thickness*2 + gap,
+                                        m_Params->get_double_param("length") * cm / 2., 0, twopi);
+  double steplimits = m_Params->get_double_param("steplimits") * cm;
+  G4UserLimits *g4userlimits = nullptr;
+  if (isfinite(steplimits))
+  {
+    g4userlimits = new G4UserLimits(steplimits);
+  }
+
+  G4LogicalVolume *cylinder_logic = new G4LogicalVolume(cylinder_solid,
+                                                        G4Material::GetMaterial("G4_AIR"),
+                                                        G4String(GetName())
+                                                        );
+  G4LogicalVolume *cylinder_logic_C = new G4LogicalVolume(cylinder_solid_C,
+                                                        TrackerMaterial,
+                                                        G4String(GetName())+"CTileLogic",
+                                                        nullptr, nullptr, g4userlimits);
+  G4LogicalVolume *cylinder_logic_Z = new G4LogicalVolume(cylinder_solid_Z,
+                                                        TrackerMaterial,
+                                                        G4String(GetName())+"ZTileLogic",
+                                                        nullptr, nullptr, g4userlimits);
+  PHG4Subsystem *mysys = GetMySubsystem();
+  mysys->SetLogicalVolume(cylinder_logic);
+  new G4PVPlacement(0, G4ThreeVector(0,0,0),
+                    cylinder_logic_C,
+                    G4String(GetName())+"CTilePhys",
+                    cylinder_logic, 0, false, OverlapCheck());
+  new G4PVPlacement(0, G4ThreeVector(0,0,0),
+                    cylinder_logic_Z,
+                    G4String(GetName())+"ZTilePhys",
+                    cylinder_logic, 0, false, OverlapCheck());
+  m_CylinderPhysicalVolume = new G4PVPlacement(0, G4ThreeVector(m_Params->get_double_param("place_x") * cm, m_Params->get_double_param("place_y") * cm, m_Params->get_double_param("place_z") * cm),
+                                               cylinder_logic,
+                                               G4String(GetName()),
+                                               logicWorld, 0, false, OverlapCheck());
+}
---
+++ source/PHG4CylinderStripDetector.h
@@ -0,0 +1,44 @@
+// Tell emacs that this is a C++ source
+//  -*- C++ -*-.
+#ifndef G4DETECTORS_PHG4CYLINDERSTRIPDETECTOR_H
+#define G4DETECTORS_PHG4CYLINDERSTRIPDETECTOR_H
+
+#include <g4main/PHG4Detector.h>
+
+#include <string>
+
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+class PHCompositeNode;
+class PHG4Subsystem;
+class PHParameters;
+
+class PHG4CylinderStripDetector : public PHG4Detector
+{
+ public:
+  //! constructor
+  PHG4CylinderStripDetector(PHG4Subsystem *subsys, PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam, const int layer = 0);
+
+  //! destructor
+  virtual ~PHG4CylinderStripDetector()
+  {
+  }
+
+  //! construct
+  void ConstructMe(G4LogicalVolume *world);
+
+  bool IsInCylinder(const G4VPhysicalVolume *) const;
+  void SuperDetector(const std::string &name) { m_SuperDetector = name; }
+  const std::string SuperDetector() const { return m_SuperDetector; }
+  int get_Layer() const { return m_Layer; }
+
+ private:
+  PHParameters *m_Params;
+
+  G4VPhysicalVolume *m_CylinderPhysicalVolume;
+
+  int m_Layer;
+  std::string m_SuperDetector;
+};
+
+#endif
